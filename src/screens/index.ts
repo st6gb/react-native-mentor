761e662867554a6249541d8ce544903aec87ba38
Pavel Buldakov <pavel_buldakov@epam.com>
2019-06-24 16:55:58 +0400

    2 second monday

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,3 @@
 export { Login } from './Login/Login';
 export { ProductList } from './ProductList/ProductList';
 export { ProductDetails } from './ProductDetails/ProductDetails';
-export { Plain } from './Plain/Plain';
